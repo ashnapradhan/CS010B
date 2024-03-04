009a275185ae1c17da13ceda14244a60008baa9d
ashnapradhan <162086940+ashnapradhan@users.noreply.github.com>
2024-03-03 22:08:32 -0800

    Create main.cpp

---
+++ PA6/main.cpp
@@ -0,0 +1,14 @@
+#include "IntList.h"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "Enter a test number(1-5): ";
+	int test;
+	cin >> test;
+	cout << endl;
+   
+   
+   return 0;
+}
